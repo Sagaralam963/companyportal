33d14a2a2361933673a540088a4a75710136a5e1
SAGAR ALAM <sagaralam963@gmail.com>
2026-02-04 07:07:41 +0000

    04/02/26 Json data bind to table and Fragment of "Create" created and open&Close in controller.

--- webapp/controller/Page1.controller.js
+++ webapp/controller/Page1.controller.js
@@ -5,6 +5,36 @@ sap.ui.define([
 
     return Controller.extend("companyportal.controller.Page1", {
         onInit() {
+            var sUrl = sap.ui.require.toUrl("companyportal/model/data.json");
+
+            var oModel = new sap.ui.model.json.JSONModel();
+            oModel.loadData(sUrl);
+
+            this.getView().setModel(oModel, "ProductModel");
+
+
+        },
+
+
+        onAddPress: function (oEvent) {
+
+
+            if (!this.oMPDialog) {
+                this.oMPDialog = this.loadFragment({
+                    name: "companyportal.fragment.Create"
+                });
+            }
+            this.oMPDialog.then(function (oDialog) {
+                this.oDialog = oDialog;
+                this.oDialog.open();
+               
+            }.bind(this));
+
+
+        },
+
+        closeDialog:function(oEvent){
+            this.oDialog.close();
         }
     });
 });
